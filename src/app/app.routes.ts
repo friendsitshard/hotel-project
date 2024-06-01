41d621a6d14d59b03693f48548abe4d5b042c39a
friendsitshard <tsiklauri2016@mail.ru>
2024-06-01 17:29:55 +0400

    Routes and some css styling added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'welcome', component:WelcomeComponent},
+    {path:'rooms', component:RoomsComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'', redirectTo:'/welcome', pathMatch:'full'},
+];

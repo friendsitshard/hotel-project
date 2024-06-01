0ee4fbff2fd16188f61063c33c7f75aaf2f177b8
friendsitshard <tsiklauri2016@mail.ru>
2024-06-01 23:46:08 +0400

    get images for carousel from api

--- src/app/welcome/carousel/carousel.component.ts
+++ src/app/welcome/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AxiosService } from '../../services/axios.service';
 
 @Component({
   selector: 'app-carousel',
@@ -8,30 +9,37 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
-export class CarouselComponent {
-  images = [
-    'assets/reception.jpg',
-    'assets/reception.jpg',
-    'assets/reception.jpg',
-    'assets/reception.jpg',
-    'assets/reception.jpg',
-  ];
-
+export class CarouselComponent implements OnInit{
+  images: string[] = [];
   currentImageIndex = 0;
 
-  showNextImage() {
+  constructor(private axiosService: AxiosService) {}
+
+  ngOnInit(): void {
+    this.axiosService.getData('https://ketiketelauri123-001-site1.jtempurl.com/api/roomtypes/getallroomtypewithimages')
+      .subscribe({
+        next: (data) => {
+          this.images = data.map((item: { image: any; }) => item.image); // Assuming 'data' is an array of objects with 'image' properties
+        },
+        error: (error) => {
+          console.error('Error fetching images:', error);
+        }
+      });
+  }
+
+  showNextImage(): void {
     this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
   }
 
-  showPreviousImage() {
+  showPreviousImage(): void {
     if (this.currentImageIndex > 0) {
       this.currentImageIndex--;
     } else {
-      this.currentImageIndex = this.images.length - 1;
+      this.currentImageIndex = this.images.length - 1; // Wrap around to the last image
     }
   }
 
-  setCurrentImageIndex(index: number) {
+  setCurrentImageIndex(index: number): void {
     this.currentImageIndex = index;
   }
   

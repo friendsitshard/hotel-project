d058e4d8f7ee8fd0010c186cf5ad1126b1d3f5a3
George Kalandadze <kalandadzeg777@gmail.com>
2024-06-02 21:03:10 +0400

    use pipes, solve carousel width

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AxiosService } from '../services/axios.service';
 import { NgFor, NgIf } from '@angular/common';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, CommonModule],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css',
 })
